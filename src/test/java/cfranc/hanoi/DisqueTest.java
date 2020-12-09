b9b59eb96719e7fcacde4bd368673f726d3e9ced
youssef-t <yousseftaleb1999@gmail.com>
2020-12-09 15:27:15 +0100

    Test added for disque and tour - ILC2 Youssef TALEB

--- src/test/java/cfranc/hanoi/DisqueTest.java
+++ src/test/java/cfranc/hanoi/DisqueTest.java
@@ -11,19 +11,62 @@ public class DisqueTest {
 	Disque dTall = new Disque(3);
 
 	@Test
-	public void testCompareTo() {
-		Disque petit = new Disque(1);
-		Disque moyen = new Disque(2);
-		Disque grand = new Disque(3);
-		assertEquals(-1, petit.compareTo(moyen));
-		assertEquals(-1, moyen.compareTo(grand));
-		//TODO : à compléter
-	}
-
-	@Test
 	public void compareTo_SmallMedium_Negative(){
 		int expected = -1;
 		int actual = dSmall.compareTo(dMedium);
 		assertEquals(expected, actual);
 	}
+
+	@Test
+	public void compareTo_MediumTall_Negative(){
+		int expected = -1;
+		int actual = dMedium.compareTo(dTall);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void compareTo_SmallTall_Negative(){
+		int expected = -1;
+		int actual = dSmall.compareTo(dTall);
+		assertEquals(expected, actual);
+	}
+	@Test
+	public void compareTo_TallSmall_Positive(){
+		int expected = 1;
+		int actual = dTall.compareTo(dSmall);
+		assertEquals(expected, actual);
+	}
+	@Test
+	public void compareTo_TallMedium_Positive(){
+		int expected = 1;
+		int actual = dTall.compareTo(dMedium);
+		assertEquals(expected, actual);
+	}
+	@Test
+	public void compareTo_MediumSmall_Positive(){
+		int expected = 1;
+		int actual = dMedium.compareTo(dSmall);
+		assertEquals(expected, actual);
+	}
+
+	//ces trois tests ne sont pas utiles pour la tour d'Hanoi(disques de taille différente)
+	@Test
+	public void compareTo_TallTall_Null(){
+		int expected = 0;
+		int actual = dTall.compareTo(dTall);
+		assertEquals(expected, actual);
+	}
+	@Test
+	public void compareTo_MediumMedium_Null(){
+		int expected = 0;
+		int actual = dMedium.compareTo(dMedium);
+		assertEquals(expected, actual);
+	}
+	@Test
+	public void compareTo_SmallSmall_Null(){
+		int expected = 0;
+		int actual = dSmall.compareTo(dSmall);
+		assertEquals(expected, actual);
+	}
+
 }
--- src/test/java/cfranc/hanoi/TourTest.java
+++ src/test/java/cfranc/hanoi/TourTest.java
@@ -6,9 +6,49 @@ import org.junit.Test;
 
 public class TourTest {
 
-	@Test
-	public void test() {
+	Disque d1 = new Disque(1);
+	Disque d2 = new Disque(2);
+	Tour _TourNonVide = new Tour(3);
 
+	@Test
+	//GIVEN_WHEN_THEN
+	public void testEmpiler_TourPleine_FALSE() {
+		Tour _TourPleine = new Tour(1);
+		_TourPleine.empiler(d2);
+
+		boolean expected = false;
+		boolean actual = _TourPleine.empiler(d1);
+
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	//GIVEN_WHEN_THEN
+	public void testEmpiler_TourNonVideDisquePlusPetit_TRUE() {
+		_TourNonVide.empiler(d2);
+
+		boolean expected = true;
+		boolean actual = _TourNonVide.empiler(d1);
+
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testEmpiler_TourNonVideDisquePlusGrand_FALSE() {
+		_TourNonVide.empiler(d1);
+
+		boolean expected = false;
+		boolean actual = _TourNonVide.empiler(d2);
+
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testEmpiler_TourVide_TRUE(){
+		Tour _TourVide = new Tour(2);
+		boolean expected = true;
+		boolean actual = _TourVide.empiler(d1);
+		assertEquals( expected, actual);
 	}
 
 }
